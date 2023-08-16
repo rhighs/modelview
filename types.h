b183b64c37a4c2a69bf92c2155e4ba1f9e85e657
Roberto Montalti <roberto.montalti00@gmail.com>
2023-08-16 19:59:15 +0200

    Custom types file

---
+++ types.h
@@ -0,0 +1,17 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef _Bool                   bool;
+typedef float                   f32;
+typedef double                  f64;
+typedef long double             f128;
+typedef unsigned char           u8;
+typedef unsigned short int      u16;
+typedef unsigned int            u32;
+typedef unsigned long long int  u64;
+typedef char                    i8;
+typedef signed short int        i16;
+typedef int                     i32;
+typedef long long int           i64;
+
+#endif // TYPES_H
